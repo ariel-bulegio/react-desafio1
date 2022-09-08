a0c3baac1c42d495f5eec0802743f1f48a0da593
Ariel <arielmaita90@gmail.com>
2022-09-07 23:47:44 -0300

    2do desafio

--- src/App.js
+++ src/App.js
@@ -3,22 +3,46 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
+    <header>
+      <nav className="navbar navbar-expand-lg navbar-light bg-light">
+  <div className="container-fluid">
+    <a className="navbar-brand" href="#">SkinS - DOTA</a>
+    <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
+      <span className="navbar-toggler-icon"></span>
+    </button>
+    <div className="collapse navbar-collapse" id="navbarNavDropdown">
+      <ul className="navbar-nav mx-auto">
+        <li className="nav-item">
+          <a className="nav-link active" aria-current="page" href="#">Sinks</a>
+        </li>
+        <li className="nav-item">
+          <a className="nav-link active" aria-current="page" href="#">Trucos</a>
+        </li>
+        <li className="nav-item">
+          <a className="nav-link active" aria-current="page" href="#">Estrategias</a>
+        </li>
+        <li className="nav-item">
+          <a className="nav-link active" aria-current="page" href="#">Ayuda</a>
+        </li>
+      </ul>
+      <ul className="navbar-nav ms-auto">
+      <li className="nav-item">
+          <a className="nav-link active" aria-current="page" href="#">Entrar</a>
+          
+        </li>   
+        <li className="nav-item">
+        <button type="button" className="btn btn-dark">Registrate</button>
+        </li>   
+        
+        
+        
+     </ul>
     </div>
+  </div>
+</nav>
+    </header>
+    
   );
 }
 
