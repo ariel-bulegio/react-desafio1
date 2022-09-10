8ce933eb1da1e7aaf2d42f704ece2a9db48715b4
Ariel <arielmaita90@gmail.com>
2022-09-09 23:46:15 -0300

    3er desafio

--- src/App.js
+++ src/App.js
@@ -1,48 +1,16 @@
 import logo from './logo.svg';
 import './App.css';
+import Navbar from './components/Navbar'; 
+import ItemListContainer from './components/ItemListContainer';
 
-function App() {
-  return (
 
-    <header>
-      <nav className="navbar navbar-expand-lg navbar-light bg-light">
-  <div className="container-fluid">
-    <a className="navbar-brand" href="#">SkinS - DOTA</a>
-    <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
-      <span className="navbar-toggler-icon"></span>
-    </button>
-    <div className="collapse navbar-collapse" id="navbarNavDropdown">
-      <ul className="navbar-nav mx-auto">
-        <li className="nav-item">
-          <a className="nav-link active" aria-current="page" href="#">Sinks</a>
-        </li>
-        <li className="nav-item">
-          <a className="nav-link active" aria-current="page" href="#">Trucos</a>
-        </li>
-        <li className="nav-item">
-          <a className="nav-link active" aria-current="page" href="#">Estrategias</a>
-        </li>
-        <li className="nav-item">
-          <a className="nav-link active" aria-current="page" href="#">Ayuda</a>
-        </li>
-      </ul>
-      <ul className="navbar-nav ms-auto">
-      <li className="nav-item">
-          <a className="nav-link active" aria-current="page" href="#">Entrar</a>
-          
-        </li>   
-        <li className="nav-item">
-        <button type="button" className="btn btn-dark">Registrate</button>
-        </li>   
-        
-        
-        
-     </ul>
-    </div>
-  </div>
-</nav>
-    </header>
-    
+const App = () => {
+
+  return(
+    <>
+    <Navbar/>
+    <ItemListContainer greeting = "Desafio Cumplido"/>
+    </>
   );
 }
 
---
+++ src/components/Navbar.js
@@ -0,0 +1,47 @@
+import { GrAid } from "react-icons/gr" ;   
+const Navbar = (props) =>{
+    return (
+  
+  <>
+        <nav className="navbar navbar-expand-lg navbar-light bg-light">
+    <div className="container-fluid">
+      <a className="navbar-brand" href="#">SkinS - DOTA</a>
+      <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
+        <span className="navbar-toggler-icon"></span>
+      </button>
+      <div className="collapse navbar-collapse" id="navbarNavDropdown">
+        <ul className="navbar-nav mx-auto">
+          <li className="nav-item">
+            <a className="nav-link active" aria-current="page" href="#">Sinks</a>
+          </li>
+          <li className="nav-item">
+            <a className="nav-link active" aria-current="page" href="#">Trucos</a>
+          </li>
+          <li className="nav-item">
+            <a className="nav-link active" aria-current="page" href="#">Estrategias</a>
+          </li>
+          <li className="nav-item">
+            <a className="nav-link active" aria-current="page" href="#">Ayuda</a>
+          </li>
+        </ul>
+        <ul className="navbar-nav ms-auto">
+        <li className="nav-item">
+            <a className="nav-link active" aria-current="page" href="#">Entrar</a>
+            
+          </li>   
+          <li className="nav-item">
+          <button type="button" className="btn btn-dark">Registrate</button>
+          </li>   
+          <GrAid className="carrito"/>
+       </ul>
+       
+      </div>
+    </div>
+  </nav>
+ 
+      </>
+  
+    )
+  }
+
+  export default Navbar
